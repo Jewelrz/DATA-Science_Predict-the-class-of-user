9c16493b6d0dabb636385242e98e545c19ebb16a
Rui Zhu <ruizhu@Ruis-MacBook-Pro.local>
2019-04-18 22:41:30 -0400

    import data

---
+++ data_clean.py
@@ -0,0 +1,12 @@
+import pandas as pd
+data2017q1 = "2017Q1tripdata.csv"
+data2017q2 ="2017Q2-capitalbikeshare-tripdata.csv"
+data2017q3 = "2017Q3-capitalbikeshare-tripdata.csv"
+data2017q4 = "2017Q4-capitalbikeshare-tripdata.csv"
+loan_df_q1 = pd.read_csv(data2017q1)
+loan_df_q2 = pd.read_csv(data2017q2)
+loan_df_q3 = pd.read_csv(data2017q3)
+loan_df_q4 = pd.read_csv(data2017q4)
+
+
+
